99aa2c243855fc80733ac1b59f41af6e9ab56b75
Bettina Nathalie Blasberg <bettina.blasberg@yahoo.de>
2015-10-28 17:07:54 +0100

    Komplette Bearbeitung der CSS Datei, damit es die letzten Anforderungen auch erfüllt

--- WME Übung 1/aufgabe1.js
+++ WME Übung 1/aufgabe1.js
@@ -13,52 +13,37 @@ function turnImageToColor()
 	image.style.cursor = "default";
 }
 
-//Boolean-Werte für die Buttons
-var isCol1Visible = true;
-var isCol2Visible = true;
-var isCol3Visible = true;
-var isCol4Visible = true;
-var isCol5Visible = true;
-var isCol6Visible = true;
-var isCol7Visible = true;
+//Funktion, um das Menü in der mobilen Version zu togglen
+var menu;
+var isClosed = true
+
+function toggleMenu()
+{
+	menu = document.getElementById('menu_links');
+	if(isClosed)
+	{
+		menu.style.display = "block";
+		isClosed = false;
+	}
+	else if(!isClosed)
+	{
+		menu.style.display = "none";
+		isClosed = true;
+	}
+}
 
 //Funktion für das Erscheinen und Verschwinden der einzelnen Tabellenzeilen
-function toggleColumn(colNumber)
+function toggleColumn(colID)
 {
-	var columnBool;
-	var column;
-	
-	switch(colNumber)
-	{
-	case 1: columnBool = isCol1Visible; column = document.getElementById('col1'); break;
-	case 2: columnBool = isCol2Visible; column = document.getElementById('col2'); break;
-	case 3: columnBool = isCol3Visible; column = document.getElementById('col3'); break;
-	case 4: columnBool = isCol4Visible;	column = document.getElementById('col4'); break;
-	case 5: columnBool = isCol5Visible; column = document.getElementById('col5'); break;
-	case 6: columnBool = isCol6Visible; column = document.getElementById('col6'); break;
-	case 7: columnBool = isCol7Visible; column = document.getElementById('col7'); break;
-	}
-	
-	if(columnBool)
+	var column = document.getElementById(colID)
+	var style = window.getComputedStyle(column, null);
+	if(style.getPropertyValue("visibility") == "visible")
 	{
 		column.style.visibility = "collapse";
-		columnBool = false;
 	}
-	else if(!columnBool)
+	else if(style.getPropertyValue("visibility") == "collapse")
 	{
 		column.style.visibility = "visible";
-		columnBool = true;
-	}
-	
-	switch(colNumber)
-	{
-	case 1: isCol1Visible = columnBool; break;
-	case 2: isCol2Visible = columnBool; break;
-	case 3: isCol3Visible = columnBool; break;
-	case 4: isCol4Visible = columnBool; break;
-	case 5: isCol5Visible = columnBool; break;
-	case 6: isCol6Visible = columnBool; break;
-	case 7: isCol7Visible = columnBool; break;
 	}
 }
 
@@ -109,4 +94,3 @@ function sortTableData(count, dir)
 	
 	
 }
-
